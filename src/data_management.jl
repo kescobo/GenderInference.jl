b8058c73dfa7bf2fc3cbad0b27be892db51db7a3
Kevin Bonham <kevbonham@gmail.com>
2019-03-20 05:43:03 -0400

    create name generation functions

--- src/data_management.jl
+++ src/data_management.jl
@@ -1,8 +1,34 @@
+function update_genders!(d::Dict, name, year, gender, n)
+    if !haskey(d, name)
+        d[name] = Dict{Int, Dict}()
+    end
+    if !haskey(d[name], year)
+        d[name][year] = Dict{Symbol, Int}()
+    end
+    haskey(d[name][year], gender) && error("duplicate $gender entry for $name $year")
+
+    d[name][year][gender] = n
+end
+
+
 function generate_names()
     datfolder = datadep"US Census - names"
-    name_dict = Dict()
-    for y in filter(f-> occursin(r"^yob\d{4}", f), datfolder)
-        table = CSV.File(joinpath(datfolder, y))
-    end
+    genders = Dict{String, Dict}()
 
+    for y in filter(f-> occursin(r"^yob\d{4}", f), readdir(datfolder))
+        year = match(r"yob(\d{4})\.txt", y).captures[1] |> x -> parse(Int, x)
+
+        for line in eachline(joinpath(datfolder, y))
+            (name, gender, n) = split(line, ',')
+
+            gender in ("M", "F") || error("Gender $gender not recognized")
+            gender == "F" ? gender = :female : gender = :male
+
+            n = parse(Int, n)
+
+            update_genders!(genders, name, year, gender, n)
+        end
+
+    end
+    return genders
 end
