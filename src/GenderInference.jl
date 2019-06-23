46ac8482a73201c9de333c9ec57f7f3f6f9aca37
Kevin Bonham <kevbonham@gmail.com>
2019-06-23 13:16:42 -0400

    switch to project

--- src/GenderInference.jl
+++ src/GenderInference.jl
@@ -12,25 +12,25 @@ using DataDeps
 using BSON
 using Logging
 
-register(DataDep(
-    "US Census - names",
-    """US Census data, 1880-2017.
-        https://catalog.data.gov/dataset/baby-names-from-social-security-card-applications-national-level-data""",
-    "https://www.ssa.gov/oact/babynames/names.zip",
-    post_fetch_method=file->run(`unzip $file`)
-    ))
+# register(DataDep(
+#     "US Census - names",
+#     """US Census data, 1880-2017.
+#         https://catalog.data.gov/dataset/baby-names-from-social-security-card-applications-national-level-data""",
+#     "https://www.ssa.gov/oact/babynames/names.zip",
+#     post_fetch_method=file->run(`unzip $file`)
+#     ))
 
-# function __init__()
-#     register(DataDep(
-#         "US Census - names",
-#         """US Census data, 1880-2017.
-#             https://catalog.data.gov/dataset/baby-names-from-social-security-card-applications-national-level-data""",
-#         "https://www.ssa.gov/oact/babynames/names.zip",
-#         post_fetch_method=file->run(`unzip $file`)
-#         ))
-#     datadep"US Census - names"
-#     nothing
-# end
+function __init__()
+    register(DataDep(
+        "US Census - names",
+        """US Census data, 1880-2017.
+            https://catalog.data.gov/dataset/baby-names-from-social-security-card-applications-national-level-data""",
+        "https://www.ssa.gov/oact/babynames/names.zip",
+        post_fetch_method=file->run(`unzip $file`)
+        ))
+    datadep"US Census - names"
+    nothing
+end
 
 include("data_management.jl")
 include("inference.jl")
