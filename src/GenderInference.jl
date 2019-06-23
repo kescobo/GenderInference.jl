3eb98717399c806af94e2b075cf75d1e22154e66
Kevin Bonham <kevbonham@gmail.com>
2019-06-23 13:51:57 -0400

    move registration to file

--- src/GenderInference.jl
+++ src/GenderInference.jl
@@ -15,9 +15,4 @@ include("data_registrations.jl")
 include("data_management.jl")
 include("inference.jl")
 
-function __init__()
-    datadep"US Census - names"
-    nothing
-end
-
 end # module Gender Inference
--- src/data_registrations.jl
+++ src/data_registrations.jl
@@ -5,3 +5,5 @@ register(DataDep(
     "https://www.ssa.gov/oact/babynames/names.zip",
     post_fetch_method=file->run(`unzip $file`)
     ))
+
+datadep"US Census - names"
