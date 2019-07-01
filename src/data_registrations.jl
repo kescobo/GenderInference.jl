6e107daaa3954b293cb4cff6959531ee4e9b7aca
Lyndon White <lyndon.white@invenialabs.co.uk>
2019-07-01 07:10:40 +0100

    Normalize DataDep handling

--- src/data_registrations.jl
+++ src/data_registrations.jl
@@ -1,7 +1,11 @@
-register(DataDep(
-    "US Census - names",
-    """US Census data, 1880-2017.
-        https://catalog.data.gov/dataset/baby-names-from-social-security-card-applications-national-level-data""",
-    "https://www.ssa.gov/oact/babynames/names.zip",
-    post_fetch_method=file->run(`unzip $file`)
-    ))
+
+# This will be called by __init__
+function init_us_census()
+    register(DataDep(
+        "US Census - names",
+        """US Census data, 1880-2017.
+            https://catalog.data.gov/dataset/baby-names-from-social-security-card-applications-national-level-data""",
+        "https://www.ssa.gov/oact/babynames/names.zip",
+        post_fetch_method=unpack
+        ))
+end
